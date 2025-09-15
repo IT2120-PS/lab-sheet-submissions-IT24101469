955d90d1b4b0065ab7c46b78a52c735dc2f6b5c9
IT24101469 <it24101469@my.sliit.lk>
2025-09-15 11:51:07 +0530

    IT24101469_lab6

---
+++ Lab Sheet 06/IT24101469.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\ASUS\\Desktop\\PS\\Lab6")
+##Exercise 1
+##part 1
+##binomial distribution with n=50 and p=0.85
+
+##part2
+pbinom(46,50,0.85,lower.tail = FALSE)
+
+##Exercise 2
+##part1
+## X=number of customer calls receives in call center on given hours
+
+##part2
+## poisson distribution, lamda=12
+
+##part3
+dpois(15,12)
